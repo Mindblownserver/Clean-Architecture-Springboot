3a3ceff26057f0754f44d9a28854e0bd681ff1ef
Mindblownserver <med.yassine.kharrat@gmail.com>
2026-04-08 07:24:01 +0100

    feat: Turned clean Monolithic architecture to cleaner modular one. - Each module has its own pom.xml, pointed to parent, and follow the **dependency rule** of clean architecture (outer layers **depend** on inner ones) - Added UserManagementServiceApplication to be the starting point of our microservice

---
+++ user-management-service/application/src/main/java/com/ums/application/UserManagementServiceApplication.java
@@ -0,0 +1,24 @@
+package com.ums.application;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.persistence.autoconfigure.EntityScan;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
+
+@SpringBootApplication
+@ComponentScan(basePackages = {
+        "com.ums.application",
+        "com.ums.usecases",
+        "com.ums.infrastructure"
+})
+// Important to detect JPA Entities
+@EntityScan(basePackages = "com.ums.infrastructure.persistence.entities")
+// Necessary to make beans of JpaRepositories
+@EnableJpaRepositories(basePackages = "com.ums.infrastructure.persistence.repository")
+public class UserManagementServiceApplication {
+    public static void main(String[] args){
+        SpringApplication.run(UserManagementServiceApplication.class);
+
+    }
+}
