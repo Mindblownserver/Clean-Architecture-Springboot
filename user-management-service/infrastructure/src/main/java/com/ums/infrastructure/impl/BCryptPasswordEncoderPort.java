d512f3927ed876f89c74324b64c3a8de667f7a40
Mindblownserver <med.yassine.kharrat@gmail.com>
2026-04-08 07:10:43 +0100

    feat: Added class that implements password hashing in infrastructure layer It uses mindrot's jbcrypt instead of spring security's.

---
+++ user-management-service/infrastructure/src/main/java/com/ums/infrastructure/impl/BCryptPasswordEncoderPort.java
@@ -0,0 +1,25 @@
+package com.ums.infrastructure.impl;
+
+
+import com.ums.usecases.ports.PasswordEncoderPort;
+import lombok.RequiredArgsConstructor;
+import org.mindrot.jbcrypt.BCrypt;
+
+@RequiredArgsConstructor
+public class BCryptPasswordEncoderPort implements PasswordEncoderPort {
+    private final int logRounds;
+    public BCryptPasswordEncoderPort(){
+        logRounds= 10;
+    }
+
+    @Override
+    public String encodePassword(String passwordInClear) {
+        String salt = BCrypt.gensalt(logRounds);
+        return BCrypt.hashpw(passwordInClear, salt);
+    }
+
+    @Override
+    public boolean passwordCmp(String passwordInClear, String passwordHash) {
+        return BCrypt.checkpw(passwordInClear, passwordHash);
+    }
+}
